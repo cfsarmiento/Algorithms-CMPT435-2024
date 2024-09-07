892adfc5632627a3c479153d8547c46a4f9984d4
cfsarmiento <97933462+cfsarmiento@users.noreply.github.com>
2024-09-07 17:28:26 -0400

    Linked list class made + part of stack class
    Linked list class is made - not so confident in myData being a string might have to change later - a bridge we will cross later :)
    
    Stack class is kind of made but am running into an error with types - still getting used to how they work in the context of typescript. Prevail we must.

---
+++ Assignment 1/NodeLinkedList.ts
@@ -0,0 +1,59 @@
+class NodeLinkedList {
+
+    // Instance variables
+    myData: string | null;
+    myNext: NodeLinkedList | null;
+
+    /* Constructor */
+    /**
+     * Constructor for linked list node class
+     * 
+     * @param data data for the node
+     * @param next next node linked to current node (if any)
+     */
+    constructor(data: string | null = null, next: NodeLinkedList | null = null) {
+
+        this.myData = data;
+        this.myNext = next;
+
+    } // constructor
+
+    /* Getters */
+    /**
+     * Class method to get data from current node
+     */
+    get getData() {
+
+        return this.myData;
+
+    } // getData()
+
+    /**
+     * Class method to get next node
+     */
+    get getNext() {
+
+        return this.myNext;
+
+    } // getNext()
+
+    /* Setters */
+    /**
+     * Class method to update data in current node
+     */
+    set setData(newData: string) {
+
+        this.myData = newData;
+
+    } // setData()
+
+    /**
+     * Class method to update next node
+     */
+    set setNext(newNext: NodeLinkedList | null) {
+
+        this.myNext = newNext;
+
+    } // setNext()
+
+} // Class Node
