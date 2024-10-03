956a70fe81a4a3eedf29b2d960adc67a9e289c7b
cfsarmiento <97933462+cfsarmiento@users.noreply.github.com>
2024-10-02 21:20:18 -0400

    Wrote sorts + shuffle
    Tested that the shuffle works and I wrote the selection & insertion sort implementations. Just have to finish merge and quick sort but I have to figure out how to divide lists up without slicing them since thats not how vectors work in C++. I would finish it right now but I am hungry!!

--- Assignment_1/C++/main.cpp
+++ Assignment_1/C++/main.cpp
@@ -5,7 +5,7 @@
  * the list in various ways using selection	sort, insertion sort, merge sort, and quick sort, shuffiling it every time
  * and counting the number of comparisons.
  * Date Created: 9/28/24
- * Last Updated: 10/1/24
+ * Last Updated: 10/2/24
  * Compilation: g++ -std=c++11 -o PalindromeFinder main.cpp NodeLinkedList.cpp Queue.cpp Stack.cpp
  * Run Program: ./PalindromeFinder
  * -----------------------------------------------------------------------------------------------------------------------
@@ -19,10 +19,132 @@
 #include <fstream>
 #include <algorithm>
 #include <cctype>
+#include<cstdlib>  // random numbers for shuffle
 
 // Types
 #include <string>
 #include <vector>  // C++ dynamic list
+
+/* Helper Methods */
+
+void swap(std::string& item1, std::string& item2) {
+
+    // Variables
+    std::string temp = " ";
+
+    // Swap
+    temp = item1;
+    item1 = item2;
+    item2 = temp;
+
+} // swap
+
+void knuthShuffle(std::vector<std::string>& strings) {
+
+    // Variables
+    int endPos = strings.size() - 1;
+    srand((unsigned) time(NULL));  // setting seed value
+
+    // Shuffle the array
+    for (int i = endPos; i >= 0; i--) {
+
+        int j = rand() % (i + 1);
+        swap(strings[i], strings[j]);
+
+    } // for 
+
+}; // knuthShuffle()
+
+int selectionSort(std::vector<std::string>& items) {
+
+    // Variables
+    int numComparisons = 0;
+    int arrayLength = items.size();
+
+    // Anchor on an element
+    for (int i=0; i <= arrayLength - 2; i++) {
+
+        // Find the minimum
+        int currentMin = i;
+        for (int j=i+1; j <= arrayLength - 1; j++) {
+
+            // Swap only if the number is less than the current mininum
+            numComparisons++;
+            if (items[j] < items[currentMin]) {
+
+                currentMin = j;
+
+            } // if
+
+        } // for j
+
+        // Swap after finding the minimum
+        swap(items[i], items[currentMin]);
+
+    } // for i
+
+    return numComparisons;
+
+} // selectionSort()
+
+int insertionSort(std::vector<std::string>& items) {
+
+    // Variables
+    std::string currentElement = " ";
+    int numComparisons = 0;
+
+    // Anchor on an element
+    for (int i=0; i < items.size() - 1; i++) {
+
+        // Move elements if they are bigger than the current element
+        currentElement = items[i];
+        int j = i - 1;
+        numComparisons++;
+        while (j >= 0 && items[j] > currentElement) {
+
+            items[j+1] = items[j];
+            j--;
+
+        } // while 
+
+        // Move current element into proper place
+        items[j+1] = currentElement;
+
+    } // for i
+
+    return numComparisons;
+
+} // insertionSort()	
+
+int mergeSort(std::vector<std::string>& items) {
+
+    // Variables
+    std::vector<std::string> lowerHalf = items;
+    std::vector<std::string> upperHalf = items;
+    if (items.size() > 1) {
+
+        // Divide the list into two halves and sort
+        int m = items.size() / 2;
+        std::vector<std::string> slice(lowerHalf.begin() + 0, lowerHalf.begin() + m);
+        mergeSort(lowerHalf);
+        std::vector<std::string> slice(upperHalf.begin() + (m + 1), upperHalf.begin() + items.size());
+        mergeSort(upperHalf);
+
+        // Merge the lists
+        mergeHelper();
+
+
+    } // if
+} // mergeSort()
+
+void mergeHelper(std::vector<std::string>& array) {
+
+
+} // mergeHelper
+
+int quickSort(std::vector<std::string>& items) {
+
+} // quickSort()
 
 int main() {
 
@@ -32,6 +154,11 @@ int main() {
     char character1 = ' ';
     char character2 = ' ';
     int palindromeCount = 0;
+    int selectionSortComparisons = 0;
+    int insertionSortComparisons = 0;
+    int mergeSortComparisons = 0;
+    int quickSortComparisons = 0;
+
 
     /* Read in items from magicitems.txt into an array */
     // Open magicitems.txt file
@@ -119,29 +246,27 @@ int main() {
 
     /* Sorting */
 
+    // Selection sort
+    knuthShuffle(magicItems);
+    selectionSortComparisons = selectionSort(magicItems);
+    std::cout << "Selection Sort Total Comparisons: " << selectionSortComparisons << std::endl;
+
+    // Insertion sort
+    knuthShuffle(magicItems);
+    insertionSortComparisons = insertionSort(magicItems);
+    std::cout << "Insertion Sort Total Comparisons: " << insertionSortComparisons << std::endl;
+
+    // Merge sort
+    knuthShuffle(magicItems);
+    mergeSortComparisons = mergeSort(magicItems);
+    std::cout << "Merge Sort Total Comparisons: " << mergeSortComparisons << std::endl;
+
+    // Quick sort
+    knuthShuffle(magicItems);
+    quickSortComparisons = quickSort(magicItems);
+    std::cout << "Quick Sort Total Comparisons: " << quickSortComparisons << std::endl;
+
 
     return 0;
 
 }; // main()
-
-/* Helper Methods*/
-
-void shuffle(std::vector<std::string> strings) {
-
-}; // shuffle()
-
-void selectionSort() {
-
-} // selectionSort()
-
-void insertionSort() {
-
-} // insertionSort()	
-
-void mergeSort() {
-
-} // mergeSort()
-
-void quickSort() {
-
-} // quickSort()
